2d0d2deba2265e5cd8d96e61888b4d8a3b7097c4
Shanel <shanelhickmanwhitmore@gmail.com>
2022-09-13 09:59:00 -0400

    getting quilty

--- digital-quilt/script.js
+++ digital-quilt/script.js
@@ -1,41 +1,37 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
+
 const make = (squares) => {
-for (let i = 1; i <= squares; i++) {
-				//create element
-		const div = document.createElement('div');
-		document.createElement([div.className = 'square']);
-		div.innerText = `${i}`;
-        div.style.width = "50px";
-        div.style.height = "50px";
-        body.style.display = 'flex';
-        body.style.flexWrap = "wrap";
-        div.style.display = "flex";
-        div.style.border = "1px";
-        div.style.borderColor = black;
-        const randColorRGB = () => {
-                let r = Math.floor(Math.random()*256);
-                let g = Math.floor(Math.random()*256);
-                let b = Math.floor(Math.random()*256);
-                return `rgb(${r},${g},${b})`;
-            }   
-            
-                div.style.backgroundColor = randColorRBG();
-                
-        
-            }
-            
-            body.appendChild(div);
-				//console.log(div);
-		
-		//append element to the DOM
-		//get the element to append to
-		
-		
-        
-		}
+  for (let i = 1; i <= squares; i++) {
+    //create element
+    const div = document.createElement("div");
+    //add square class
+    document.createElement([(div.className = "square")]);
+    //uncomment line below to see how many divs are squares
+    //console.log(div.classList.contains('square'))
+    //style the square
+    div.innerText = `${i}`;
+    div.style.width = "5px";
+    div.style.height = "5px";
+    body.style.display = "flex";
+    body.style.flexWrap = "wrap";
+    div.style.display = "flex";
+    div.style.border = "solid";
+    div.style.borderColor = "#ffffff";
+    //to hard code the random background color uncomment below
+    // div.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+    div.style.backgroundColor = randomRGB();
+    //append element to the DOM
+    body.appendChild(div);
+  }
+  //function generates a random RGB value
+  function randomRGB() {
+    return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+      Math.random() * 256
+    )},${Math.floor(Math.random() * 256)})`;
+  }
+  // console.log(div);
 
-
-
-
-
+  //get the element to append to
+};
+//run function to create num squares
 make(500);
