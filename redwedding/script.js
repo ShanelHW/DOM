c890001cac705ab3d86329d4c67abb9d2cb39a9d
Shanel <shanelhickmanwhitmore@gmail.com>
2022-09-14 09:17:38 -0400

    Red wedding

---
+++ redwedding/script.js
@@ -0,0 +1,15 @@
+// Create Two Functions -> addChild and redWedding
+let addChild = () => {}
+let redWedding = () => {}
+
+// Attach functions to buttons
+
+let addChildBtn = document.getElementById('add-child');
+addChildBtn.addEventListener('click', addChild);
+
+let redWeddingBtn = document.getElementById('red-wedding');
+redWeddingBtn.addEventListener('click', redWedding);
+
+// The addChild function creates new div & appends it to the starks div
+
+// The redWedding function removes ALL stark children (loop)
