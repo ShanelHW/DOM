75af688cf9ccfe699bba632ab6a456098ce776d7
Shanel <shanelhickmanwhitmore@gmail.com>
2022-09-14 13:51:34 -0400

    Update script.js

--- eventbubbling/script.js
+++ eventbubbling/script.js
@@ -2,6 +2,11 @@ const ul = document.querySelector('ul')
 const li = document.querySelector('li')
 const body = document.querySelector('body')
 
+const createli = () =>{
+    const li = document.createElement('li')
+    li.addEventListener('click', () => console.log('New Li'))
+    return li;
+}
 li.addEventListener('click', function(e){
     console.dir(e);
     console.log(e.target);
